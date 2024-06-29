45b337fd7d4f9b09de1c9be73755c10b990508e9
ADI <adiletaktay@gmail.com>
2024-06-29 20:37:29 +0500

    fix deploy gh pages

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -6,60 +6,60 @@ import ShowFullItem from "./ShowFullItem"
 class MainPage extends React.Component {
   constructor(props) {
     super(props)
-    this.state ={
+    this.state = {
       currentItems: [],
       items: [
         {
           id: 1,
-          title: 'Стол директора',
-          img: './img/1.jpg',
-          desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-          category: 'tables',
-          price: '99.99'
+          title: "Стол директора",
+          img: require("../img/1.jpg"),
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+          category: "tables",
+          price: "99.99",
         },
         {
           id: 2,
-          title: 'Стуль для офиса',
-          img: './img/k1.jpg',
-          desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-          category: 'chairs',
-          price: '59.99'
+          title: "Стуль для офиса",
+          img: require("../img/k1.jpg"),
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+          category: "chairs",
+          price: "59.99",
         },
         {
           id: 3,
-          title: 'Мягкая мебель',
-          img: './img/426(1)-288x220w.jpg',
-          desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-          category: 'sofa',
-          price: '89.99'
+          title: "Мягкая мебель",
+          img: require("../img/426(1)-288x220w.jpg"),
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+          category: "sofa",
+          price: "89.99",
         },
         {
           id: 4,
-          title: 'Офисное пространство',
-          img: './img/7.jpg',
-          desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-          category: 'office',
-          price: '79.99'
+          title: "Офисное пространство",
+          img: require("../img/7.jpg"),
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+          category: "office",
+          price: "79.99",
         },
         {
           id: 5,
-          title: 'Стуль для приемной',
-          img: './img/k2.jpg',
-          desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-          category: 'chairs',
-          price: '49.99'
+          title: "Стуль для приемной",
+          img: require("../img/k2.jpg"),
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+          category: "chairs",
+          price: "49.99",
         },
         {
           id: 6,
-          title: 'Мебель для приемной',
-          img: './img/504(1)-288x220w.jpg',
-          desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
-          category: 'office',
-          price: '69.99'
-        }
+          title: "Мебель для приемной",
+          img: require("../img/504(1)-288x220w.jpg"),
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+          category: "office",
+          price: "69.99",
+        },
       ],
       showFullItem: false,
-      fullItem: {}
+      fullItem: {},
     }
     this.state.currentItems = this.state.items
     this.addToOrder = this.addToOrder.bind(this)
@@ -70,43 +70,43 @@ class MainPage extends React.Component {
   render() {
     return (
       <div>
-        <div className='presentation'></div>
-        <Categories chooseCategory={this.chooseCategory}/>
+        <div className="presentation"></div>
+        <Categories chooseCategory={this.chooseCategory} />
         <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder} />
 
-        {this.state.showFullItem && <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem}/>}
+        {this.state.showFullItem && (
+          <ShowFullItem onAdd={this.addToOrder} onShowItem={this.onShowItem} item={this.state.fullItem} />
+        )}
       </div>
     )
   }
 
   onShowItem(item) {
-    this.setState({fullItem: item})
-    this.setState({showFullItem: !this.state.showFullItem})
+    this.setState({ fullItem: item })
+    this.setState({ showFullItem: !this.state.showFullItem })
   }
 
   chooseCategory(category) {
-    if (category === 'all') {
-      this.setState({currentItems: this.state.items})
+    if (category === "all") {
+      this.setState({ currentItems: this.state.items })
       return
     }
-    
+
     this.setState({
-      currentItems: this.state.items.filter(el => el.category === category)
+      currentItems: this.state.items.filter(el => el.category === category),
     })
   }
 
   addToOrder(item) {
-    const storageOrders = localStorage.getItem('orders')
+    const storageOrders = localStorage.getItem("orders")
     const orders = JSON.parse(storageOrders) || []
 
     let isInArray = false
     orders.forEach(el => {
-      if (el.id === item.id)
-        isInArray = true
+      if (el.id === item.id) isInArray = true
     })
-    if (!isInArray)
-      localStorage.setItem('orders', JSON.stringify([...orders, item]))
+    if (!isInArray) localStorage.setItem("orders", JSON.stringify([...orders, item]))
   }
 }
 
-export {MainPage};
+export { MainPage }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { RouterProvider, createHashRouter } from "react-router-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  },
+])
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
