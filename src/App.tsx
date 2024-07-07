86ebab658f69ace6ba0d9211117750d9f09c1323
ADI <adiletaktay@gmail.com>
2024-07-07 16:40:29 +0500

    add layout and fix header

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from "react"
-import Header from "./components/Header"
 import Footer from "./components/Footer"
 import { Routes, Route } from "react-router-dom"
 import { About } from "./components/About"
 import { Cabinet } from "./components/Cabinet"
 import { Contacts } from "./components/Contacts"
 import { MainPage } from "./components/MainPage"
+import MainLayout from "./components/MainLayout"
 
 class App extends React.Component {
   render() {
     return (
       <div className="wrapper">
-        <Header />
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/cabinet" element={<Cabinet />} />
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/cabinet" element={<Cabinet />} />
+          </Route>
         </Routes>
         <Footer />
       </div>
---
+++ src/components/Header.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react"
+import { FaShoppingCart } from "react-icons/fa"
+import { Link, useLocation } from "react-router-dom"
+import Order from "./Order"
+
+interface OrderItem {
+  id: number
+  img: string
+  title: string
+  price: number
+}
+
+const deleteOrder = (id: number): void => {
+  const storageOrders = localStorage.getItem("orders")
+  const orders: OrderItem[] = JSON.parse(storageOrders || "[]")
+
+  localStorage.setItem("orders", JSON.stringify(orders.filter(el => el.id !== id)))
+}
+
+const showOrders = (): JSX.Element => {
+  let summa = 0
+  const storageOrders = localStorage.getItem("orders")
+  const orders: OrderItem[] = JSON.parse(storageOrders || "[]")
+  orders.forEach(el => (summa += el.price))
+
+  return (
+    <div>
+      {orders.map(el => (
+        <Order onDelete={deleteOrder} key={el.id} item={el} />
+      ))}
+      <p className="summa">Сумма: {new Intl.NumberFormat().format(summa)}$</p>
+    </div>
+  )
+}
+
+const showNothing = (): JSX.Element => {
+  return (
+    <div className="empty">
+      <h2>Товаров нет</h2>
+    </div>
+  )
+}
+
+const Header: React.FC = () => {
+  const [cartOpen, setCartOpen] = useState<boolean>(false)
+  const location = useLocation()
+
+  return (
+    <>
+      <header>
+        <div className="container">
+          <Link to="/" className="logo header__container">
+            <span>Office Furniture</span>
+          </Link>
+          <ul className="nav">
+            <li>
+              <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>
+                Про нас
+              </Link>
+            </li>
+            <li>
+              <Link to="/contacts" className={location.pathname === "/contacts" ? "active" : ""}>
+                Контакты
+              </Link>
+            </li>
+            <li>
+              <Link to="/cabinet" className={location.pathname === "/cabinet" ? "active" : ""}>
+                Кабинет
+              </Link>
+            </li>
+          </ul>
+          <FaShoppingCart
+            onClick={() => setCartOpen(!cartOpen)}
+            className={`shop-cart-button ${cartOpen ? "active" : ""}`}
+          />
+
+          {cartOpen && (
+            <div className="shop-cart">
+              {localStorage.getItem("orders") && JSON.parse(localStorage.getItem("orders") || "[]").length > 0
+                ? showOrders()
+                : showNothing()}
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default Header
